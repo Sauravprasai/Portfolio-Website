7adb28fd4e72f729e7f48f162014e1a32419aadd
Sauravprasai <sauravprasai2016@gmail.com>
2023-06-13 22:46:06 -0400

    Updated footer

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Saurav\'s Portfolio' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('about', { title: 'About me' });
 });
 
 router.get('/project-showcase', function(req, res, next) {
-  res.render('project', { title: 'Project-showcase' });
+  res.render('project', { title: 'Project showcase' });
 });
 
 router.get('/contact-me', function(req, res, next) {
-  res.render('contact', { title: 'Contact-Me' });
+  res.render('contact', { title: 'Contact me' });
 });
 
 module.exports = router;
